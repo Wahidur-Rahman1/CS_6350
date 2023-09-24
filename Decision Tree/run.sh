b284665cdaae61aff9588de647d4e42d066b0c6c
Wahidur-Rahman1 <113296400+Wahidur-Rahman1@users.noreply.github.com>
2023-09-23 23:05:20 -0600

    Add files via upload

---
+++ Decision Tree/run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Upgrade scikit-learn, scipy, matplotlib, and pandas
+pip install -U scikit-learn scipy matplotlib pandas
+
+
+# Run first file
+echo "IG_CAR DECISION_TREE.py" 
+python3  IG_CAR DECISION_TREE.py
+
+
+
+# Run 2nd file
+echo "Running  GI_CAR DECISION_TREE.py"
+python3 GI_CAR DECISION_TREE.py
+
+
+# Run 3rd file
+echo "Running  ME_CAR DECISION_TREE.py"
+python3 ME_CAR DECISION_TREE.py
+
+# Run 4th file
+echo "IG_pruned_bank.py" 
+python3  IG_pruned_bank.py
+
+
+
+# Run 5th file
+echo "Running  GI_pruned_bank.py"
+python3  GI_pruned_bank.py
+
+
+# Run 6th file
+echo "Running   ME_pruned_bank.py"
+python3  ME_pruned_bank.py
+
+# Run 7th file
+echo "IG_Pruned_missing_value.py" 
+python3  IG_Pruned_missing_value.py
+
+
+# Run 8th file
+echo "Running  GI_Pruned_missing_value.py"
+python3 GI_Pruned_missing_value.py
+
+
+# Run 9th file
+echo "Running  ME_Pruned_missing_value.py"
+python3 ME_Pruned_missing_value.py
+
+
+
+
