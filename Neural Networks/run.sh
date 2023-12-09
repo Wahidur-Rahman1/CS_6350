adb8f8f07e5dea80db650e9358a5bf4cc55e7fb6
Wahidur-Rahman1 <113296400+Wahidur-Rahman1@users.noreply.github.com>
2023-12-09 02:37:23 -0700

    Add files via upload

---
+++ Neural Networks/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "running question 2(a)"
+python3 2_a_main.py
+
+echo "running question 2(b)"
+python3 2_b_main.py
+
+echo "running question 2(c)"
+python3 2_c.py
+
+echo "running question 2(e)"
+python3 2_e.py
